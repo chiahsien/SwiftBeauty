cf4cfac5d6ed3d7af9493e5d859caef61fe7dada
Nelson Tai <chiahsien@gmail.com>
2020-11-05 01:10:14 +0800

    Remove Alamofire

--- SwiftBeauty/Source Fetcher/CustomError.swift
+++ SwiftBeauty/Source Fetcher/CustomError.swift
@@ -9,9 +9,6 @@
 import Foundation
 
 enum CustomError: Error {
-    case network(error: Error) // Capture any underlying Error from the URLSession API
-    case dataSerialization(error: Error)
-    case parse(error: Error)
     case networkError(_: Error)
     case parseError(_: Error)
     case invalidData(_: Data)
