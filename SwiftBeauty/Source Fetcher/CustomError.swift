e97fe2585b20bbc0745290912aaee4dcc71931fd
Nelson Tai <chiahsien@gmail.com>
2020-11-05 01:02:45 +0800

    Use URLSession in TimLiao fetcher

--- SwiftBeauty/Source Fetcher/CustomError.swift
+++ SwiftBeauty/Source Fetcher/CustomError.swift
@@ -12,5 +12,8 @@ enum CustomError: Error {
     case network(error: Error) // Capture any underlying Error from the URLSession API
     case dataSerialization(error: Error)
     case parse(error: Error)
+    case networkError(_: Error)
+    case parseError(_: Error)
+    case invalidData(_: Data)
     case emptyData
 }
